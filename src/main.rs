44c23659c4e6dd927a1e8209d40a45eae58793c0
justin <justinsweng@gmail.com>
2023-07-30 22:39:41 -0400

    use wait_try

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,40 @@
 #![allow(unused)]
 fn main() {
-use std::process::Command;
-use log::{info, error, LevelFilter};
-use systemd_journal_logger::JournalLog;
+    use log::{error, info, LevelFilter};
+    use std::process::Command;
+    use std::{thread, time};
+    use systemd_journal_logger::JournalLog;
 
-JournalLog::default()
-    .with_syslog_identifier("video_record".to_string())
-    .install().unwrap();
-log::set_max_level(LevelFilter::Info);
+    JournalLog::default()
+        .with_syslog_identifier("video_record".to_string())
+        .install()
+        .unwrap();
+    log::set_max_level(LevelFilter::Info);
 
-let mut libcamera_process = Command::new("libcamera-vid")
-	.args(["-t", "10000"])
+    let mut libcamera_process = Command::new("libcamera-vid")
+        .args(["-t", "10000"])
         .args(["-o", "/media/ssd-drive/test.h264"])
         .spawn();
 
-match libcamera.try_wait() {
-   Ok(mut child) => {
-     match child.stdout {
-       None => error!("stdout not defined"),
-       Some(_) =>{ let stdout = child.stdout.take().unwrap(); info!("{:?}", stdout) },
-     }
-   },
-   Err(error) => error!("problem spawning command: {:?}", error),
-};
+    match libcamera_process {
+        Ok(mut handle) => {
+            info!("spawn libcamera process...");
 
-match libcamera_process.try_wait() {
-    Ok(Some(status)) => warn!("exited with: {status}"),
-    Ok(None) => {
-      println!("status not ready yet, lets wait");
-      
-    }
-}
-
-    
+            loop {
+                match handle.try_wait() {
+                    Ok(Some(status)) => {
+                        info!("exited with: {status}");
+                        break;
+                    }
+                    Ok(None) => {
+                        println!("status not ready yet, lets wait");
+                        let ten_millis = time::Duration::from_millis(10);
+                        thread::sleep(ten_millis);
+                    }
+                    Err(e) => error!("error attempting to wait: {e}"),
+                }
+            }
+        }
+        Err(error) => error!("problem spawning command: {:?}", error),
+    };
 }
